8a310d522f532bfbbe5432c8614f75cb69767e32
Maike Heuner <heuner@bafg.de>
2017-04-06 14:21:17 +0200

    updated github configuration

---
+++ dev.bat
@@ -0,0 +1,14 @@
+E:
+cd tibass\data\stem_density
+git init
+git add -A
+git commit -m "first commit"
+git config --global user.name "Maike Heuner"
+git config --global user.email heuner@bafg.de 
+git config --global http.sslVerify false 
+REM git remote add gitbucket https://apps.bafg.de/gitbucket/git/heuner/stem_density.git
+REM git remote add github https://github.com/heuner/stem_density.git
+REM git push -u gitbucket master
+REM git push -u github master
+pause
+exit
